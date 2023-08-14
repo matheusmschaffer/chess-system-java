d61e5f6c22dec7514878c620bf881dcb83d5bb85
Matheus Schaffer <teuzin@gmail.com>
2023-08-14 14:42:05 -0300

    From 7 commit of chess-system

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,40 @@
+package application;
+
+
+import boardgame.BoardException;
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                System.out.println();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                System.out.println();
+            }
+        }
+
+
+    }
+}
